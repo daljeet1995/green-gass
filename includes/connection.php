121b2a7b482089f5330770af66a34c097f61c23d
daljeet1995 <ds025272@gmail.com>
2018-10-09 16:53:21 +0530

    Green Gass

---
+++ includes/connection.php
@@ -0,0 +1,14 @@
+<?php
+require("constants.php");
+// 1. Create a database connection
+$connection = mysqli_connect(server,user,pass);
+if (!$connection) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+// 2. Select a database to use 
+$db_select = mysqli_select_db($connection, name);
+if (!$db_select) {
+    die("Database selection failed: " . mysqli_error($connection));
+}
+?>
